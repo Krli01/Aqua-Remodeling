7db89bd04883b009a07154ed5d1e1c42e6edc8f8
Karla Díaz <129137641+Krli01@users.noreply.github.com>
2026-04-13 11:48:14 -0400

    test deploy

--- src/components/sections/index.ts
+++
@@ -1,4 +0,0 @@
-export { HeroSection } from "./hero-section";
-export { ServicesSection } from "./services-section";
-export { FAQSection } from "./faq-section";
-export { ContactSection } from "./contact-section";
